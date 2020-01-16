2c5752d6f619c70cb549e8b53566c59bec80ad75
jmarqs99 <8180551@estg.ipp.pt>
2020-01-16 15:18:33 +0000

    Map and Jogo

--- src/Classes/Mapas.java
+++ src/Classes/Mapas.java
@@ -5,10 +5,26 @@
  */
 package Classes;
 
+import EstruturasDeDados.Network;
+import org.json.simple.*;
+
 /**
  *
  * @author Utilizador
  */
 public class Mapas {
-    
+
+    private final String NOME;
+    private final int PONTOS;
+    private Network<String> aposentos;
+
+    public Mapas(JSONObject mapa) {
+        this.PONTOS = (int) mapa.get("pontos");
+        this.NOME = (String) mapa.get("nome");
+        Object map[] = (Object[]) mapa.get("mapa");
+        for (int i = 0; i < map.length; i++) {
+            Object object = map[i];
+            
+        }
+    }
 }
