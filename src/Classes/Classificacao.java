610d355c19064c444b679b7fc0f44daab623b9b8
jmarqs99 <8180551@estg.ipp.pt>
2020-01-26 20:11:12 +0000

    interface e classe

--- src/Classes/Classificacao.java
+++ src/Classes/Classificacao.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interfaces;
+package Classes;
 
 /**
  *
  * @author Utilizador
  */
-public interface IMapas {
- 
-    
+public class Classificacao {
     
 }
